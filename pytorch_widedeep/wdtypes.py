61231c0ff197dd9794a49bdba5325a9bf3938f61
jrzaurin <jrzaurin@gmail.com>
2019-09-30 20:01:53 +0100

    adding type Iterable

--- pytorch_widedeep/wdtypes.py
+++ pytorch_widedeep/wdtypes.py
@@ -3,7 +3,7 @@ from torch.optim.optimizer import Optimizer
 from torch.utils.data.dataloader import DataLoader
 from torch.optim.lr_scheduler import _LRScheduler
 from pathlib import PosixPath
-from typing import List, Any, Union, Dict, Optional, Tuple, Generator, Collection
+from typing import List, Any, Union, Dict, Optional, Tuple, Generator, Collection, Iterable
 
 LRScheduler = _LRScheduler
 ModelParams = Generator[Tensor,Tensor,Tensor]
