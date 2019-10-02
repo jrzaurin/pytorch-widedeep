100f92c901f3386e9aa8eac6a5e0bf6626434e1d
jrzaurin <jrzaurin@gmail.com>
2019-10-02 19:56:03 +0100

    adding new types

--- pytorch_widedeep/wdtypes.py
+++ pytorch_widedeep/wdtypes.py
@@ -1,9 +1,18 @@
+from torch.nn import Module
 from torch import Tensor
+from torchvision.transforms import *
 from torch.optim.optimizer import Optimizer
 from torch.utils.data.dataloader import DataLoader
 from torch.optim.lr_scheduler import _LRScheduler
 from pathlib import PosixPath
 from typing import List, Any, Union, Dict, Optional, Tuple, Generator, Collection, Iterable
 
+Transforms= Union[CenterCrop, ColorJitter, Compose, FiveCrop, Grayscale,
+	Lambda, LinearTransformation, Normalize, Pad, RandomAffine,
+	RandomApply, RandomChoice, RandomCrop, RandomGrayscale,
+	RandomHorizontalFlip, RandomOrder, RandomResizedCrop, RandomRotation,
+	RandomSizedCrop, RandomVerticalFlip, Resize, Scale, TenCrop,
+	ToPILImage, ToTensor]
+TorchModel = Module
 LRScheduler = _LRScheduler
 ModelParams = Generator[Tensor,Tensor,Tensor]
