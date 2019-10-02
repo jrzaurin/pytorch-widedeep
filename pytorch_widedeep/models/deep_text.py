aff6008d42177421de0669b891f94c1a64edf1d2
jrzaurin <jrzaurin@gmail.com>
2019-10-02 19:47:17 +0100

    renaming parameters for consistency and fixing a bug related to the embedding dimensions

--- pytorch_widedeep/models/deep_text.py
+++ pytorch_widedeep/models/deep_text.py
@@ -1,4 +1,3 @@
-import pdb
 import numpy as np
 import torch
 import torch.nn as nn
@@ -8,7 +7,7 @@ from ..wdtypes import *
 
 
 class DeepText(nn.Module):
-    def __init__(self, vocab_size:int, embedding_dim:int, hidden_dim:int, n_layers:int,
+    def __init__(self, vocab_size:int, embed_dim:int, hidden_dim:int, n_layers:int,
         rnn_dropout:float, spatial_dropout:float, padding_idx:int, output_dim:int,
         bidirectional:bool=False, embedding_matrix:Optional[np.ndarray]=None,
         pretrained:bool=True):
@@ -18,12 +17,15 @@ class DeepText(nn.Module):
         """
         self.bidirectional = bidirectional
         self.spatial_dropout = spatial_dropout
-        self.word_embedding_dropout = nn.Dropout2d(spatial_dropout)
-        self.word_embedding = nn.Embedding(vocab_size, embedding_dim, padding_idx = padding_idx)
+        self.word_embed_dropout = nn.Dropout2d(spatial_dropout)
         if isinstance(embedding_matrix, np.ndarray):
-            self.word_embedding.weight = nn.Parameter(torch.Tensor(embedding_matrix))
+            self.word_embed = nn.Embedding(vocab_size, embedding_matrix.shape[1], padding_idx = padding_idx)
+            self.word_embed.weight = nn.Parameter(torch.Tensor(embedding_matrix))
+            embed_dim = embedding_matrix.shape[1]
+        else:
+            self.word_embed = nn.Embedding(vocab_size, embed_dim, padding_idx = padding_idx)
         self.pretrained = pretrained
-        self.rnn = nn.GRU(embedding_dim,
+        self.rnn = nn.GRU(embed_dim,
             hidden_dim,
             num_layers=n_layers,
             bidirectional=bidirectional,
@@ -34,15 +36,15 @@ class DeepText(nn.Module):
 
     def forward(self, X:Tensor)->Tensor:
 
-        embedded = self.word_embedding(X)
+        embed = self.word_embed(X.long())
         # Spatial dropout: dropping an entire channel (word-vector dimension)
         if self.spatial_dropout > 0.:
-            sd_embedded = embedded.unsqueeze(2)
-            sd_embedded = sd_embedded.permute(0, 3, 2, 1)
-            sd_embedded = self.word_embedding_dropout(sd_embedded)
-            sd_embedded = sd_embedded.permute(0, 3, 2, 1)
-            embedded = sd_embedded.squeeze(2)
-        o, h = self.rnn(embedded)
+            sd_embed = embed.unsqueeze(2)
+            sd_embed = sd_embed.permute(0, 3, 2, 1)
+            sd_embed = self.word_embed_dropout(sd_embed)
+            sd_embed = sd_embed.permute(0, 3, 2, 1)
+            embed = sd_embed.squeeze(2)
+        o, h = self.rnn(embed)
         if self.bidirectional:
             last_h = torch.cat((h[-2], h[-1]), dim = 1)
         else:
