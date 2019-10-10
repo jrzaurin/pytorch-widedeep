98014867162c8afb47b4ea1191d7b0c8adebdd39
jrzaurin <jrzaurin@gmail.com>
2019-10-10 10:03:53 +0100

    refactored code to a class with fit and transform methods

--- pytorch_widedeep/utils/wide_utils.py
+++ pytorch_widedeep/utils/wide_utils.py
@@ -1,27 +1,58 @@
 import numpy as np
 import pandas as pd
 
-from sklearn.preprocessing import StandardScaler
+from sklearn.preprocessing import OneHotEncoder
 
 from ..wdtypes import *
 
 
-def prepare_wide(df:pd.DataFrame, wide_cols:List[str], crossed_cols,
-    already_dummies:Optional[List[str]]=None) -> np.ndarray:
+class PrepareWide(object):
+    def __init__(self, wide_cols:List[str], crossed_cols=None,
+        already_dummies:Optional[List[str]]=None):
+        super(PrepareWide, self).__init__()
+        self.wide_cols = wide_cols
+        self.crossed_cols = crossed_cols
+        self.already_dummies = already_dummies
+        self.one_hot_enc = OneHotEncoder(sparse=False)
 
-    df_wide = df.copy()[wide_cols]
+    def cross_cols(self, df:pd.DataFrame):
+        crossed_colnames = []
+        for cols in self.crossed_cols:
+            cols = list(cols)
+            for c in cols: df[c] = df[c].astype('str')
+            colname = '_'.join(cols)
+            df[colname] = df[cols].apply(lambda x: '-'.join(x), axis=1)
+            crossed_colnames.append(colname)
+        return df, crossed_colnames
 
-    crossed_columns = []
-    for cols in crossed_cols:
-        cols = list(cols)
-        colname = '_'.join(cols)
-        df_wide[colname] = df_wide[cols].apply(lambda x: '-'.join(x), axis=1)
-        crossed_columns.append(colname)
+    def fit(self, df:pd.DataFrame)->np.ndarray:
 
-    if already_dummies:
-        dummy_cols = [c for c in wide_cols+crossed_columns if c not in already_dummies]
-    else:
-        dummy_cols = wide_cols+crossed_columns
-    df_wide = pd.get_dummies(df_wide, columns=dummy_cols)
+        df_wide = df.copy()[self.wide_cols]
+        if self.crossed_cols is not None:
+            df_wide, crossed_colnames = self.cross_cols(df_wide)
+            self.wide_crossed_cols = self.wide_cols + crossed_colnames
 
-    return df_wide.values
+        if self.already_dummies:
+            X_oh_1 = self.df_wide[self.already_dummies].values
+            dummy_cols = [c for c in self.wide_crossed_cols if c not in self.already_dummies]
+            X_oh_2=self.one_hot_enc.fit_transform(df_wide[dummy_cols])
+            return np.hstack((X_oh_1, X_oh_2))
+        else:
+            return self.one_hot_enc.fit_transform(df_wide[self.wide_crossed_cols])
+
+    def transform(self, df:pd.DataFrame)->np.ndarray:
+
+        df_wide = df.copy()[self.wide_cols]
+        if self.crossed_cols is not None:
+            df_wide, _ = self.cross_cols(df_wide)
+
+        if self.already_dummies:
+            X_oh_1 = self.df_wide[self.already_dummies].values
+            dummy_cols = [c for c in self.wide_crossed_cols if c not in self.already_dummies]
+            X_oh_2=self.one_hot_enc.transform(df_wide[dummy_cols])
+            return np.hstack((X_oh_1, X_oh_2))
+        else:
+            return self.one_hot_enc.transform(df_wide[self.wide_crossed_cols])
+
+    def fit_transform(self, df:pd.DataFrame)->np.ndarray:
+        return self.fit(df)
