384c817cae5e6fced8fe55f04c2b08d45e5ec092
jrzaurin <jrzaurin@gmail.com>
2019-10-16 10:48:00 +0100

    adapted the code to compute the number of classes internally

--- pytorch_widedeep/losses.py
+++ pytorch_widedeep/losses.py
@@ -4,10 +4,10 @@ import torch.nn.functional as F
 
 from .wdtypes import *
 
+
 class FocalLoss(nn.Module):
-    def __init__(self, num_classes:int, alpha:float=0.25, gamma:float=1.):
+    def __init__(self, alpha:float=0.25, gamma:float=1.):
         super().__init__()
-        self.num_classes = num_classes
         self.alpha = alpha
         self.gamma = gamma
 
@@ -16,10 +16,14 @@ class FocalLoss(nn.Module):
         p = x.sigmoid()
         pt = p*t + (1-p)*(1-t)
         w = self.alpha*t + (1-self.alpha)*(1-t)
-        return w * (1-pt).pow(self.gamma)
+        return (w * (1-pt).pow(self.gamma)).detach()
 
     def forward(self, input:Tensor, target:Tensor) -> Tensor:
-        binary_target = torch.eye(self.num_classes)[target.data.cpu()]
+        if input.size(1) == 1:
+            input = torch.cat([1-input, input], axis=1)
+            num_class = 2
+        else: num_class = input.size(1)
+        binary_target = torch.eye(num_class)[target.data.cpu().long()]
         binary_target = binary_target.contiguous()
         weight = self.get_weight(input, binary_target)
-        return F.binary_cross_entropy_with_logits(input, binary_target, weight, reduction='sum')/self.num_classes
+        return F.binary_cross_entropy_with_logits(input, binary_target, weight, reduction='sum')/num_class
