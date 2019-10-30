63700e2105bff1fe60b5a662ef3afcab5cffb3ec
jrzaurin <jrzaurin@gmail.com>
2019-10-30 11:56:14 +0000

    added version attribute and made utils accessible from pytorch-widedeep

--- pytorch_widedeep/__init__.py
+++ pytorch_widedeep/__init__.py
@@ -1,1 +1,19 @@
-from .utils import *
+##################################################
+# Store Library Version
+##################################################
+import os.path
+
+try:
+    with open(os.path.join(os.path.dirname(__file__), "VERSION")) as f:
+        __version__ = f.read().strip()
+except Exception:
+    raise
+
+
+##################################################
+# utils module accessible from pytorch-widedeep
+##################################################
+from .utils import dense_utils
+from .utils import text_utils
+from .utils import fastai_transforms
+from .utils import image_utils
