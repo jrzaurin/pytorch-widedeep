541730dd406cffb54da964734331e425dabd4068
thatalfredh <alfred.h.haowen@gmail.com>
2023-04-25 18:21:44 +0800

    added oversample_mul into dataloader args

--- pytorch_widedeep/training/trainer.py
+++ pytorch_widedeep/training/trainer.py
@@ -1204,6 +1204,7 @@ class Trainer(BaseTrainer):
             "generator",
             "prefetch_factor",
             "persistent_workers",
+            "oversample_mul",
         ]
         lds_params = [
             "lds_kernel",
